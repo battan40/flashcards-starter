ce0a486be3b55e0c341d52d2310fa795461d9246
Angela Battillo <angela.battillo@gmail.com>
2021-03-24 18:47:34 -0600

    Initiate class and test file structures

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,15 @@
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    // this.currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+
+describe('Game', () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+it('should be a function', () => {
+  expect(Game).to.be.a('function');
+})
+
+});
